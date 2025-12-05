7ce82263bf76765a5550a26d4d6251a3fe645dda
Rizwanakm <you@example.com>
2025-12-05 09:47:24 +0530

    backend

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/weatherdb");
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error(error);
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err);
     process.exit(1);
   }
 };
